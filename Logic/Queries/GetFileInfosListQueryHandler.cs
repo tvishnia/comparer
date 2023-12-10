c5e2832d0b645132785a9d4f7123aaf47aa93499
Tatiana Vishniakova <72614436+akawildcat@users.noreply.github.com>
2023-12-10 16:57:38 +0100

    Add file statuses (#19)
    * added File status to FileInfo
    
    * migration
    
    * get files list
    
    * add default values to pagination
    change from IQueriable to IReadOnlyCollection

---
+++ Logic/Queries/GetFileInfosListQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using ComparerBasic.Domain;
+using ComparerBasic.Infrastructure;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComparerBasic.Logic.Queries;
+
+public record GetFileInfosListQuery(
+    int PageNum = 1,
+    int PageSize = 100
+) : IRequest<IReadOnlyCollection<SingleFileInfo>>;
+public class GetFileInfosListQueryHandler : IRequestHandler<GetFileInfosListQuery, IReadOnlyCollection<SingleFileInfo>>
+{
+    private readonly IComparerContext _dbContext;
+    
+    public GetFileInfosListQueryHandler(IComparerContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<IReadOnlyCollection<SingleFileInfo>> Handle(GetFileInfosListQuery query, CancellationToken cancellationToken)
+    {
+        var list = await _dbContext.SingleFileInfos
+            .OrderBy(x => x.FileName)
+            .Skip((query.PageNum - 1) * query.PageSize)
+            .Take(query.PageSize)
+            .ToListAsync(cancellationToken);
+        
+        return list; 
+    } 
+}
+
