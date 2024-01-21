6d915c2cb77bb2dd4341fa103968b1e23629d2f6
Tatiana Vishnyakova <akawildcat@gmail.com>
2024-01-21 22:19:00 +0100

    migration

---
+++ Migrations/20240121211815_FolderInfos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComparerBasic.Migrations
+{
+    /// <inheritdoc />
+    public partial class FolderInfos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FolderInfos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FolderName = table.Column<string>(type: "text", nullable: false),
+                    FolderStatus = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FolderInfos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FolderInfos");
+        }
+    }
+}
